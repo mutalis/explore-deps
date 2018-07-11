fb4ed682e31476dd905505b01dcd1ddcf53960f4
Jessica Kerr <jessitron@gmail.com>
2018-07-10 19:21:06 -0500

    blah

--- build/cli/explore.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ts-node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const youAreIn_1 = require("./support/youAreIn");
-youAreIn_1.youAreIn(process.cwd());
-console.log("everything is fucked here");
