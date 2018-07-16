f710b3832d408e8f5f40e84c5cce7f4748855405
Jessica Kerr <jessitron@gmail.com>
2018-07-16 10:32:40 -0500

    wrap in function

--- src/cli/explore.ts
+++ src/cli/explore.ts
@@ -1,16 +1,20 @@
-#!/usr/bin/env ts-node
+#!/usr/bin/env node
 
 import { buildRoom } from "./support/buildRoom";
 import { output, outputDoom } from "./support/output";
 import { timeToAct } from "./support/timeToAct";
 import { itsaTrap } from "./support/Trap";
 
-buildRoom(process.cwd()).then((room) => {
-    if (itsaTrap(room)) {
-        outputDoom("Ouch! " + room.details);
-        return;
-    }
-    return timeToAct(room, []).then(() => {
-        output("The end.");
+function explore(startingDir: string) {
+    buildRoom(startingDir).then((room) => {
+        if (itsaTrap(room)) {
+            outputDoom("Ouch! " + room.details);
+            return;
+        }
+        return timeToAct(room, []).then(() => {
+            output("The end.");
+        });
     });
-});
+}
+
+explore(process.cwd());
