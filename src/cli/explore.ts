95a2c87ccc506710236ae4a1890d8e91ca6b66b1
Jessica Kerr <jessitron@gmail.com>
2018-07-16 17:00:09 -0500

    top level async

--- src/cli/explore.ts
+++ src/cli/explore.ts
@@ -2,19 +2,20 @@
 
 import { buildRoom } from "../support/buildRoom";
 import { itsaTrap } from "../support/Trap";
-import { output, outputDoom } from "./output";
+import { output, outputDoom, outputDebug } from "./output";
 import { timeToAct } from "./timeToAct";
 
-function explore(startingDir: string) {
-    buildRoom(startingDir).then((room) => {
-        if (itsaTrap(room)) {
-            outputDoom("Ouch! " + room.details);
-            return;
-        }
-        return timeToAct(room, []).then(() => {
-            output("The end.");
-        });
-    });
+async function explore(startingDir: string) {
+    const room = await buildRoom(startingDir);
+    if (itsaTrap(room)) {
+        outputDoom("Ouch! " + room.details);
+        return;
+    }
+    await timeToAct(room, []);
+    output("The end.");
 }
 
-explore(process.cwd());
+explore(process.cwd()).catch(err => {
+    outputDoom("Unhandled exception: " + err.message);
+    outputDebug(err.stack);
+});
