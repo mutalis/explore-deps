19bab44eb1f5b2925ab566c3b44ad431006f5f8e
Jessica Kerr <jessitron@gmail.com>
2018-07-16 11:09:19 -0500

    move test next to file

--- src/support/describeMove.test.ts
+++ src/support/describeMove.test.ts
@@ -1,29 +1,29 @@
 import * as assert from "assert";
-import { describeMove } from "../../../src/cli/support/describeMove";
+import { describeMove } from "./describeMove";
 
 describe("color text for moving to another directory", () => {
 
-    it("move into an adjacent lib", () => {
+    it("calls a move into an adjacent lib 'step across' ", () => {
         const result = describeMove("/Users/jessitron/code/other/yargs/node_modules/chai",
             "/Users/jessitron/code/other/yargs/node_modules/ucui");
         assert.strictEqual(result, "You step across into ucui.");
     });
 
-    it("move down one", () => {
+    it("calls a move down one 'descend'", () => {
         const result = describeMove("/Users/jessitron/code/other/yargs",
             "/Users/jessitron/code/other/yargs/node_modules/ucui");
         assert.strictEqual(result, "You descend into ucui.");
     });
 
-    it("move into a global lib"); // how does this work? it doesn't look in the places that my libs go
+    it("calls move into a global lib 'whoosh'"); // I don't even know how to install something globally except by hand
 
-    it("move up some random directories", () => {
+    it("calls a move up some random directories 'twisted stairs'", () => {
         const result = describeMove("/Users/jessitron/code/other/yargs",
             "/Users/jessitron/code/node_modules/@atomist/automation-client");
         assert.strictEqual(result, "You climb 2 twisted stairs, then descend into automation-client.");
     });
 
-    it("move up some node_module directories", () => {
+    it("calls a move up some node_module directories 'climb stairs and step'", () => {
         const result = describeMove("/Users/jessitron/code/other/yargs/node_modules/chai/node_modules/subchai",
             "/Users/jessitron/code/other/yargs/node_modules/ucui");
         assert.strictEqual(result, "You climb one flight of stairs and step into ucui.");
